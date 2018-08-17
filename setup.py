c927511b86f7b75fad83ffb7593b416f42f82ec6
Max Edell <mgedell@gmail.com>
2018-08-16 20:46:05 -0400

    update setup

--- setup.py
+++ setup.py
@@ -6,29 +6,18 @@ with open('README.md') as readme_file:
 required = []
 
 setup(
-    name='SimpleSensor',
+    name='diyHue',
     version='0.1.0',
-    description='A Python IoT framework for simple sensor integration',
+    description='',
     long_description=readme,
-    author='',
-    author_email='',
-    packages=[  'simplesensor', 
-                'simplesensor.shared', 
-                'simplesensor.cli', 
-                'simplesensor.collection_modules', 
-                'simplesensor.collection_modules.demographic_camera',
-                'simplesensor.communication_modules', 
-                'simplesensor.communication_modules.websocket_server',
-                'simplesensor.communication_modules.mqtt_client'
+    packages=[  'diyhue',
+                'diyhue.utils',
+                'diyhue.nginx',
+                'diyhue.config',
+                'diyhue.bridge'
                 ],
-    package_data= {
-                    'simplesensor': ['config/*.conf', '../logs/*'],
-                    'simplesensor.collection_modules.demographic_camera': ['config/*.conf', 'classifiers/haarcascades/*.xml'],
-                    'simplesensor.communication_modules.websocket_server': ['config/*.conf'],
-                    'simplesensor.communication_modules.mqtt_client': ['config/*.conf']
-                    },
     install_requires=required,
     entry_points = {
-        'console_scripts': ['scly=simplesensor.cli.main:main']
+        'console_scripts': ['diy=TODO']
     }
 )
